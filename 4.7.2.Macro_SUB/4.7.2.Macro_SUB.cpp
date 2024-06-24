5fd7580ef69d939dd3eab056aed313c9ee807fbe
Mumb <alik.sanya.07@mail.ru>
2024-06-24 18:03:17 +0300

    Project added

---
+++ 4.7.2.Macro_SUB/4.7.2.Macro_SUB.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+#define SUB(a,b) ((a) - (b))
+
+int main()
+{
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+}
